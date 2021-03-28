85dbe9aad08b7b3ae1db1c2b4f54494902014e21
spawn-dev <spawn-dev>
2021-03-28 11:59:16 -0500

    allow init for individual environments

--- scripts/init.sh
+++ scripts/init.sh
@@ -5,21 +5,13 @@ set -eo pipefail
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 PROJECT_DIR="$SCRIPT_DIR/.."
 
+ENV=$1
+
 echo "installing dependencies"
 yarn install --frozen-lockfile
 echo "building component"
 npm run build
-echo "provisioning dev site"
-npm run provision dev
-echo "deploying dev site"
-npm run deploy dev
-
-echo "provisioning stage site"
-npm run provision stage
-echo "deploying stage site"
-npm run deploy stage
-
-echo "provisioning prod site"
-npm run provision prod
-echo "deploying prod site"
-npm run deploy prod
+echo "provisioning $ENV site"
+npm run provision "$ENV"
+echo "deploying "$ENV" site"
+npm run deploy "$ENV"
