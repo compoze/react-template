08e4199166548ba05622d5f9cb57d770d6d1f3cd
spawn-dev <spawn-dev>
2021-03-28 15:41:31 -0500

    fail if no env is provided

--- scripts/init.sh
+++ scripts/init.sh
@@ -7,11 +7,16 @@ PROJECT_DIR="$SCRIPT_DIR/.."
 
 ENV=$1
 
-echo "installing dependencies"
-yarn install --frozen-lockfile
-echo "building component"
-npm run build
-echo "provisioning $ENV site"
-npm run provision "$ENV"
-echo "deploying "$ENV" site"
-npm run deploy "$ENV"
+if [[ -z "${ENV}" ]]; then
+    echo "no environment provided"
+    exit 1
+else
+    echo "installing dependencies"
+    yarn install --frozen-lockfile
+    echo "building component"
+    npm run build
+    echo "provisioning $ENV site"
+    npm run provision "$ENV"
+    echo "deploying "$ENV" site"
+    npm run deploy "$ENV"
+fi
